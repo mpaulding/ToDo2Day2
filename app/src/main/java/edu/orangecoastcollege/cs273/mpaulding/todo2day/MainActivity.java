60025ac4d35f011ef0923714ce8595a342637b70
mpaulding <mpaulding@occ.cccd.edu>
2016-10-13 12:37:51 -0700

    Removed unnecessary code.

--- app/src/main/java/edu/orangecoastcollege/cs273/mpaulding/todo2day/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/mpaulding/todo2day/MainActivity.java
@@ -40,41 +40,6 @@ public class MainActivity extends AppCompatActivity {
 
         taskEditText = (EditText) findViewById(R.id.taskEditText);
 
-        /*
-        //              ADD FIVE TASK ITEMS TO THE DATABASE
-        database.addTask(new Task(1, "Read Hamlet", 1));
-        database.addTask(new Task(2, "Study for exam", 1));
-        database.addTask(new Task(3, "Call Andy and Sam", 0));
-        database.addTask(new Task(4, "Create newsletter", 1));
-        database.addTask(new Task(5, "Buy a dog", 0));
-
-        //            DISPLAY ALL THE TASK ITEMS IN THE TABLE
-        ArrayList<Task> taskList = database.getAllTasks();
-        for (Task singleTask : taskList) {
-
-            Log.i("DATABASE RECORDS", singleTask.toString());
-        }
-
-
-        // EXPERIMENT 2: MODIFY A RECORD
-        database.editTask(new Task(1, "Read newspaper", 1));
-
-        //EXPERIMENT 3: DISPLAY A SPECIFIC RECORD
-        Task anItem = database.getTask(2);
-        Log.i("DATABASE RECORDS", anItem.toString());
-
-        //EXPERIMENT 4: DELETE A RECORD
-        database.deleteTask(new Task(
-                5, "Buy a dog", 0));
-
-        //              DISPLAY ALL THE TASK ITEMS IN THE TABLE
-
-        taskList = database.getAllTasks();
-        for (Task singleTask : taskList) {
-
-            Log.i("DATABASE RECORDS", singleTask.toString());
-        }
-        */
     }
 
     @Override
